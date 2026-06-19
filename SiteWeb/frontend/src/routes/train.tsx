5e4e8c50eda3571b41a15a946299140dcf115f8e
Teo-13 <teo.mery@outlook.com>
2026-06-19 09:59:05 +0200

    ficher venv

--- SiteWeb/frontend/src/routes/train.tsx
+++ SiteWeb/frontend/src/routes/train.tsx
@@ -1,10 +1,23 @@
 import React, { useEffect, useState } from "react";
+import "./train.css";
 
 const Train = () => {
     return (
-        <section>
-            partie train
-        </section>
+        <div>
+            <section className="bandeau-image">
+                <h1>Partie sur le Train</h1>
+                <div className="image">
+                    <img src="./src/img/train.jpeg" alt="" />
+                    <img src="./src/img/train2.jpeg" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+            </section>
+
+            <section>
+                histoire 
+            </section>
+        </div>
     );
 };
 
