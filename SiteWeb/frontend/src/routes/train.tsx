66a298afcde1aacf01373826800b532b0f88701b
Teo-13 <teo.mery@outlook.com>
2026-06-23 10:31:19 +0200

    suite du site

--- SiteWeb/frontend/src/routes/train.tsx
+++ SiteWeb/frontend/src/routes/train.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import "./train.css";
 
 const Train = () => {
+    const [step, setStep] = useState(1);
+
+
     return (
         <div>
             <section className="bandeau-image">
@@ -13,6 +16,25 @@ const Train = () => {
                     <img src="" alt="" />
                 </div>
             </section>
+
+            <section>
+               <button value={1}>{"<"}</button>
+                {step === 1 && (
+                    <div>
+                        texte
+                    </div>
+                )}
+
+                {step == 2 && (
+                    <div>
+                        carte france des gares
+                    </div>
+                )}
+
+                <button>{">"}</button>
+            </section>
+
+
 
             <section>
                 histoire 
